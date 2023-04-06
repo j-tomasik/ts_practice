fb4581d92177e101da1e124e9b232530437acf36
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-06 15:13:23 -0700

    interface for obj and vars

--- index.ts
+++ index.ts
@@ -7,3 +7,13 @@ const url = new URL('...')
 async function hello(){
     return 'world'
 }
+
+type Style = 'bold' | 'italic' | 23;
+
+let font: Style;
+
+interface Person {
+    fist: string;
+    last: string;
+    [key: string]: any;
+}
