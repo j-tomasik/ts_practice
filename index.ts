16e6929384f5102e27ff58e6989b7b4cf870cb41
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-06 15:27:54 -0700

    tuple with multiple types inside

--- index.ts
+++ index.ts
@@ -17,3 +17,23 @@ interface Person {
     last: string;
     [key: string]: any;
 }
+//use return type /void for functions that dont return a value
+function pow(x:number, y:number):string {
+    return Math.pow(x, y).toString();
+}
+
+const arr1: number[] = []
+
+arr1.push(1)
+
+//tuples
+//fixed length arr where each item has its own type
+//ts will expect the tuple to be initalized with these vals inside, avoid this
+//error message by making them optional in the decloration with ?
+type MyList = [number?, string?, boolean?]
+
+const arr2: MyList = []
+
+arr2.push(1)
+arr2.push('23')
+arr2.push(false)
